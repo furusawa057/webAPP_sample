0f28d7d3b8f733e40c43791637b7274132a16fef
furusawa057 <sfurusawa057@gmail.com>
2019-09-24 16:41:07 +0900

    render_templateでHTMLのテンプレートを読み込む

--- flask_sample.py
+++ flask_sample.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import random
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -16,6 +18,15 @@ def goodbye():
 @app.route('/add/<a>/<b>')
 def add(a, b):
     return str(int(a) + int(b))
+
+
+@app.route('/omikuji')
+def omikuji():
+    results = ['大吉', '吉', '凶']
+    # fortune_num=random.randint(0,len(result)-1)
+    result = random.choice(results)
+    # return f'今日の運勢は{result}'
+    return render_template('omikuji.html', unsei=result)
 
 
 if __name__ == '__main__':
